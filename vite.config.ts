6b2227d09e390697ed73e420f270e18b7bfc99de
Geeth2000 <geethanjankaru123@gmail.com>
2025-11-19 22:15:19 +0530

    change base image

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/my-portfolio/",
+  base: "/",
   plugins: [tailwindcss()],
 });
